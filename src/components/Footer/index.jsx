c8d4a290d6a8e8495280ec11460e4c6c743b001a
RamonSantos10 <ramon.santos14@fatec.sp.gov.br>
2025-04-08 07:54:16 -0300

    feat: Fazendo modificações na estrutura de pastas e na estilização do projeto

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <footer className="justify-center w-full text-1xl bg-blue-900 text-amber-50" >
+      <footer className="fixed bottom-0 w-full text-1xl bg-blue-900 text-amber-50">
         <p className="items-center flex justify-center">Calculadora do Agricultor Education&#174; 2025&#169;</p>
-    </footer>
+      </footer>
     </>
   )
 }
